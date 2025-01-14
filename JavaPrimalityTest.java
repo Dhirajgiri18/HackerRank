dd45c59bc8d65b8e8c8b4f5b4b313ec2ad49e0df
bhavesh977 <dhirajgiri91124@gmail.com>
2025-01-14 23:49:15 +0530

    Update prime

--- JavaPrimalityTest.java
+++ JavaPrimalityTest.java
@@ -1,29 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-
+import java.math.BigInteger;
 
 public class JavaPrimalityTest {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String n = bufferedReader.readLine();
-        int num = Integer.parseInt(n);
-        if(IsPrime(num)){
-            System.out.println("prime");
-        }else{
-            System.out.println("not prime");
-        }
-
+        String n = bufferedReader.readLine().trim();
+        BigInteger num = new BigInteger(n);
+        System.out.println(num.isProbablePrime(10) ? "prime" : "not prime");
         bufferedReader.close();
-    }
-    public static boolean IsPrime(int num){
-        for(int i=2; i<num-1; i++){
-            if(num%i==0){
-                return false;
-            }
-        }
-        return true;
     }
 }
